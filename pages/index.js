3767333dcb9c3872b53bdb5f1ebcd67ba71e8a7b
Paul Jensen <paulbjensen@gmail.com>
2020-08-09 16:30:53 +0100

    Added some tweaks to the UI color scheme for dark mode

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,29 @@ import React from 'react';
 import Link from 'next/link';
 
 const hero = {
-	marginTop: '120px',
+	marginTop: '60px',
+};
+
+const heroH1 = {
+	marginTop: '240px',
+	fontSize: '64px',
+	lineHeight: '1em',
 };
 
 const lead = {
-	fontSize: '16px',
-	fontWeight: 300,
+	fontSize: '18px',
+	fontWeight: 400,
 	marginBottom: '32px',
+};
+
+const ctas = {
+	display: 'flex',
+	width: '300px',
+	justifyContent: 'space-between',
+};
+
+const headHeadingAndLead = {
+	marginBottom: '200px',
 };
 
 // TODO - move the custom css into a dedicate scss file
@@ -17,11 +33,20 @@ const lead = {
 const HomePage = () => (
 	<div style={{ marginTop: '40px' }}>
 		<div style={hero}>
-			<h1>A Design System for React</h1>
-			<p style={lead}>Built for Anephenix</p>
-			<Link href="get-started">
-				<a className="button theme-default primary">Get started</a>
-			</Link>
+			<div style={headHeadingAndLead}>
+				<h1 style={heroH1}>A Design System for React</h1>
+				<p style={lead}>Built for Anephenix</p>
+			</div>
+			<div className="ctas" style={ctas}>
+				<Link href="get-started">
+					<a className="button theme-default primary">Get started</a>
+				</Link>
+				<Link href="docs">
+					<a className="button theme-default secondary">
+						Documentation
+					</a>
+				</Link>
+			</div>
 		</div>
 	</div>
 );
