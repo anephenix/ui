7366f66d6cc536ec87bac784322a92770ee04e92
Paul Jensen <paulbjensen@gmail.com>
2025-01-25 22:01:50 +0000

    Added unit tests for the button component

--- src/components/button/Button.test.jsx
+++ src/components/button/Button.test.jsx
@@ -1,6 +1,34 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render, fireEvent } from '@testing-library/react';
 import Button from './Button';
 
 describe('Button', () => {
-	Button;
-	test.todo('should do something');
+	test('renders with correct text', () => {
+		const { getByText } = render(<Button text="Click me" />);
+		expect(getByText('Click me')).toBeInTheDocument();
+	});
+
+	test('calls onClick when clicked', () => {
+		const handleClick = jest.fn();
+		const { getByText } = render(<Button text="Click me" onClick={handleClick} />);
+		fireEvent.click(getByText('Click me'));
+		expect(handleClick).toHaveBeenCalledTimes(1);
+	});
+
+	test('passes className to the button element', () => {
+		const { container } = render(<Button text="Click me" className="custom-class" />);
+		expect(container.firstChild).toHaveClass('custom-class');
+	});
+
+	test('forwards ref to the button element', () => {
+		const ref = React.createRef();
+		render(<Button text="Click me" ref={ref} />);
+		expect(ref.current).toBeInstanceOf(HTMLButtonElement);
+	});
+
+	test('sets the name attribute on the button element', () => {
+		const { getByRole } = render(<Button text="Click me" name="test-button" />);
+		expect(getByRole('button')).toHaveAttribute('name', 'test-button');
+	});
 });
--- src/components/radio-button/RadioButton.test.jsx
+++ src/components/radio-button/RadioButton.test.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import RadioButton from './RadioButton';
 
 describe('RadioButton', () => {
